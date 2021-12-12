d73bc6d21bfa838235a03f4b2605e662f6f2896d
Jan Radziminski <radziminski.j@gmail.com>
2021-12-12 12:33:38 +0100

    fix: fixed pgm file reading

--- demo.py
+++ demo.py
@@ -1,9 +1,13 @@
 import numpy
 
+
 def read_pgm(pgmf):
     """Return a raster of integers from a PGM as a list of lists."""
-    assert pgmf.readline() == 'P5\n'
-    (width, height) = [int(i) for i in pgmf.readline().split()]
+    first_line = pgmf.readline()
+    assert first_line == b'P5\n'
+    pgmf.readline()
+    second_line = pgmf.readline()
+    (width, height) = [int(i) for i in second_line.split()]
     depth = int(pgmf.readline())
     assert depth <= 255
 
@@ -16,11 +20,10 @@ def read_pgm(pgmf):
     return raster
 
 
-
-
 if __name__ == '__main__':
     f = open('boat.pgm', 'rb')
     pgm = read_pgm(f)
+    print(pgm)
 
 # hist, bin_edges = numpy.histogram(a=im,
 #                                   bins=range(0, 257))
