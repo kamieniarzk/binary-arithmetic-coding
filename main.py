7e2ee7c262ec19e7e3efa4086112dc28dab1beab
Jan Radziminski <radziminski.j@gmail.com>
2022-01-28 15:02:10 +0100

    sth working

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from matplotlib import pyplot as plt
 # from bitstring import BitArray
 import math
 
+int_size = 32
+b = 8
+b_bit_map = '0' * (int_size - b) + '1' * b
+halfB = 2**(b - 1)
+quarterB = 2**(b - 2)
+
 
 def calculate_binary_symbol_frequency(input_list):
     c1, c2 = 0, 0
@@ -19,7 +25,7 @@ def calculate_binary_symbol_frequency(input_list):
 
 def printB(binarty_int, variable=''):
     print_num = bin(binarty_int)[2:]
-    while len(print_num) < 8:
+    while len(print_num) < b:
         print_num = '0' + print_num
     print(variable + ': ' + print_num + ' (' + str(binarty_int) + ')')
 
@@ -28,55 +34,86 @@ def join_int_list(int_list):
     return ''.join(list(map(str, int_list)))
 
 
-def shift_left_and_fill(number, fill_bit):
-    shifted = (number << 1) & int(f'0000000011111111', 2)
-    printB(shifted, 'shifted')
-    return shifted | int(f'0000000{fill_bit}', 2)
+def shift_left_and_fill(number, fill_bit='0'):
+    shifted = (number << 1) & int(b_bit_map, 2)
+    zeros = '0' * (int_size - 1)
+    return shifted | int(f'{zeros}{fill_bit}', 2)
+
+
+def shift_left_and_add(number, add_bit):
+    shifted = (number << 1)
+    shifted += add_bit
+    return shifted & int(b_bit_map, 2)
+
+
+def rshift(val, n): return val >> n if val >= 0 else (val+0x100000000) >> n
+
+
+def getLpsClps(c0, c1):
+    if c0 < c1:
+        return ['0', c0]
+
+    return ['1', c1]
 
 
 def binary_arithmetic_encoding(input_list):
+    c0, c1 = calculate_binary_symbol_frequency(input_list)
 
-    c1, c2 = calculate_binary_symbol_frequency(input_list)
-    D = int('00000000', 2)
-    R = int('100000000', 2)
+    lps, clps = getLpsClps(c0, c1)
+
+    L = 0
+    R = 2**b
+
     LN = 0
     k = 0
+
     out_list = []
-    mult_factor = c1 / (c1 + c2)
+
+    mult_factor = clps / (c0 + c1)
 
     k = 0
-    for element in input_list:
+    for bit in input_list:
+        rLPS = math.floor(R * mult_factor)
+        rMPS = R - rLPS
 
-        R1 = math.floor(R * mult_factor)
-        R2 = R - R1
-
-        if element == '0':
-            R = R1
-            D = D + R2
+        if bit == lps:
+            L = L + rMPS
+            R = rLPS
         else:
-            R = R2
+            R = rMPS
 
-        normalizuj_kod(D, R, LN, out_list)
+        L, R, LN, out_list = encoder_renormalization(L, R, LN, out_list)
 
     k = 0
     ending = []
-    last_bit = D & int('00000001', 2)
+    last_bit = L & int('00000001', 2)
 
-    # jeśli zakodowany ciąg ma długość mniej niz 8 - doklejamy znaczące bity z D albo zera
-    while last_bit == 0 and k < 8:
-        D >>= 1
-        last_bit = D & int('00000001', 2)
-        k += 1
+    # # # jeśli zakodowany ciąg ma długość mniej niz 8 - doklejamy znaczące bity z L albo zera
+    # while last_bit == 0 and k < 8:
+    #     L = rshift(L, 1)
+    #     last_bit = L & int('00000001', 2)
+    #     k += 1
 
-    if k < 8:
-        for i in range(k, 8):
-            last_bit = D & int('00000001', 2)
-            D >>= 1
-            ending.insert(0, last_bit)
+    # if k < 8:
+    #     for i in range(k, 8):
+    #         last_bit = L & int('00000001', 2)
+    #         L = rshift(L, 1)
+    #         ending.insert(0, last_bit)
 
-        out_list += ending
-    elif len(out_list) < 8:
-        zeros_to_add = 8 - len(out_list) if len(out_list) < 8 else 0
+    # out_list += ending
+
+    endingR = []
+
+    for i in range(0, b):
+        zeros = '0' * (int_size - 1)
+        last_bit = R & int(f'{zeros}1', 2)
+        R = rshift(R, 1)
+        endingR.insert(0, last_bit)
+
+    out_list += endingR
+
+    if len(out_list) < b:
+        zeros_to_add = b - len(out_list)
         zeros_array = []
         i = 0
         while i < zeros_to_add:
@@ -84,86 +121,95 @@ def binary_arithmetic_encoding(input_list):
             i += 1
         out_list += zeros_array
 
-    return [out_list, c1, c2]
+    return [out_list, c0, c1]
 
 
-def binary_arithmetic_decoding(input_string, c1, c2):
-    POL = 1 << 7
-    CWIERC = 1 << 6
-    D = int('00000000', 2)
-    R = int('100000000', 2)
+def bit_plus_follow(bit, follow_count):
+    out_list = [bit]
+    while follow_count > 0:
+        out_list.append(str(1 - int(bit)))
+        follow_count -= 1
+    return out_list
 
-    mult_factor = c1 / (c1 + c2)
-    print(f'mult factor is {mult_factor}')
+
+def encoder_renormalization(L, R, LN, out_list):
+    while R <= quarterB:
+        if L + R <= halfB:
+            out_list += bit_plus_follow('0', LN)
+
+        elif L >= halfB:
+            out_list += bit_plus_follow('1', LN)
+            L = L - halfB
+        else:
+            LN += 1
+            L -= quarterB
+
+        L = shift_left_and_fill(L)
+        R = shift_left_and_fill(R)
+
+    return L, R, LN, out_list
+
+
+def binary_arithmetic_decoding(input_string, c0, c1):
+    L = 0
+    R = 2**b
+
+    lps, clps = getLpsClps(c0, c1)
+    N = c0 + c1
+    mult_factor = clps / (c0 + c1)
 
     input_len = len(input_string)
-    kn_initial_len = 8 if input_len > 8 else input_len
-    N = c1 + c2
-    print(f'Input is: {input_string}')
-    Kn = int(join_int_list(input_string[:kn_initial_len]), 2)
+    V_initial_len = b if input_len > b else input_len
+    V = int(join_int_list(input_string[:V_initial_len]), 2)
+
     output = []
     k = 0
-    input_string_counter = kn_initial_len
+    input_counter = V_initial_len
+
+    D = V - L
+
     while k < N:
-        R1 = math.floor(R * mult_factor)
-        R2 = R - R1
 
-        if Kn - D >= R2:
-            R = R1  # 128
-            D += R2  # 128
-            output.append('0')  # 0
+        rLPS = math.floor(R * mult_factor)
+        rMPS = R - rLPS
+
+        if D >= rMPS:
+            output.append(lps)
+            D -= rMPS
+            R = rLPS
         else:
-            R = R2
-            output.append('1')
+            output.append(str(1 - int(lps)))
+            R = rMPS
 
-        normalizuj_dek(input_string, D, R, Kn, input_string_counter)
+        input_string, D, R, V, input_counter = decoder_renormalization(
+            input_string, D, R, V, input_counter)
         k += 1
 
     return output
 
 
-def normalizuj_dek(input_string, D, R, Kn, input_string_counter):
-    POL = 1 << 7
-    CWIERC = 1 << 6
+def pobierz_bit(input_string, input_counter):
+    if input_counter >= len(input_string):
+        return 0
+    return int(input_string[input_counter])
 
-    while R <= CWIERC:
-        if D + R <= POL:
+
+def decoder_renormalization(input_string, L, R, V, input_counter):
+    while R <= quarterB:
+        if L + R <= halfB:
             pass
-        elif D >= POL:
-            D -= POL
-            Kn -= POL
+        elif L >= halfB:
+            L -= halfB
+            V -= halfB
         else:
-            D -= CWIERC
-            Kn -= CWIERC
-        D = shift_left_and_fill(D, 0)
-        R = shift_left_and_fill(R, 0)
-        Kn = shift_left_and_fill(Kn, 0)
-        Kn = shift_left_and_fill(Kn, int(
-            input_string[input_string_counter] if input_string_counter < len(input_string) else 0))
-        input_string_counter += 1
+            L -= quarterB
+            V -= quarterB
+        L = shift_left_and_fill(L)
+        R = shift_left_and_fill(R)
+        V = shift_left_and_add(V, pobierz_bit(input_string, input_counter))
+        input_counter += 1
 
-
-def normalizuj_kod(D, R, LN, out_list):
-    POL = 1 << 7
-    CWIERC = 1 << 6
-
-    while R <= CWIERC:
-        if D >= POL:
-            out_list.append(1)
-            while LN > 0:
-                out_list.append(0)
-                LN -= 1
-            D -= POL
-        elif D + R <= POL:
-            out_list.append(0)
-            while LN > 0:
-                out_list.append(1)
-                LN -= 1
-        else:
-            LN += 1
-            D -= CWIERC
-        D = shift_left_and_fill(D, 0)
-        R = shift_left_and_fill(R, 0)
+    return input_string, L, R, V, input_counter
 
 
 def test_binary_arithmetic_encoding_decoding(input_string):
@@ -188,8 +234,8 @@ def test_binary_arithmetic_encoding_decoding(input_string):
 
 
 def run_test():
-    MIN_LENGTH = 2
-    MAX_LENGTH = 6
+    MIN_LENGTH = 8
+    MAX_LENGTH = 9
     test_cases = []
 
     for length in range(MIN_LENGTH, MAX_LENGTH + 1):
@@ -204,18 +250,18 @@ def run_test():
     for case in test_cases:
         result, messages = test_binary_arithmetic_encoding_decoding(case)
         if case != result:
-            failed_cases.append(case)
+            failed_cases.append(case + ' ' + result)
         else:
             succeeded_cases.append(case)
 
     print(f'Succeed: {len(test_cases) - len(failed_cases)}')
     print(f'Failed: {len(failed_cases)}')
 
-    print('Succeed cases:')
-    print(succeeded_cases)
+    # print('Succeed cases:')
+    # print(succeeded_cases)
 
-    print('Failed cases:')
-    print(failed_cases)
+    # print('Failed cases:')
+    # print(failed_cases)
 
 
 if __name__ == '__main__':
